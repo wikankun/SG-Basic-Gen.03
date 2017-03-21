d37d4b68a3fba8cda7a678f59b870e4ff5199813
Wikan Kuncara Jati <wikan25@gmail.com>
2017-03-21 21:04:41 +0700

    Add files via upload
    awawa

--- DataLatihan/answer1.py
+++ DataLatihan/answer1.py
@@ -1,5 +1,5 @@
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
+data1 = "Data1.txt"
+data2 = "Data2.txt"
 
 def readData(data1):
 	x = []
@@ -7,3 +7,12 @@ def readData(data1):
 	    for line in data :
 		    x = line.split()
 	return x
+
+x = readData(data1)
+a = []
+for i in x:
+	if i == 'I': a.append('*')
+	elif ((i == 'and') or (i == 'the') or (i == 'you')): a.append('*'*3)
+	else: a.append(i)
+b = ' '.join(a)
+print(b)
